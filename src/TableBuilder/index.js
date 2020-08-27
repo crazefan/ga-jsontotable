9374be04954948f94acb80d18612e8c0ebfbeb95
crazefan <dolgopolovt@gmail.com>
2020-08-27 21:49:30 +0200

    utility functions and rendering first level

--- src/TableBuilder/index.js
+++ src/TableBuilder/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { getHeadLabelsFromRows } from "../utils";
+import { getHeadLabelsFromRows, getRowKids } from "../utils";
 
 import {
   Box,
@@ -11,21 +11,57 @@ import {
   IconButton,
   TableBody,
   Typography,
-  Tab,
 } from "@material-ui/core";
 
-const TableBuilder = ({ rows, onRowUpdated }) => {
+import {
+  Cancel,
+  KeyboardArrowDown,
+  KeyboardArrowRight,
+} from "@material-ui/icons";
+
+const RowBuilder = ({ rowData, onUpdated, onDeleted }) => {
+  const kids = getRowKids(rowData);
+  const { data, isOpen = false } = rowData;
+  return (
+    <>
+      <TableRow>
+        <TableCell>
+          <IconButton>{kids ? <KeyboardArrowDown /> : null}</IconButton>
+        </TableCell>
+        {Object.keys(data).map((key) => (
+          <TableCell key={`table-cell-${key}`}>{data[key]}</TableCell>
+        ))}
+        <TableCell>
+          <IconButton>
+            <Cancel />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+};
+
+const TableBuilder = ({ rows, onRowUpdated, onRowDeleted }) => {
   const headLabels = getHeadLabelsFromRows(rows);
   return (
     <Table>
       <TableHead>
         <TableRow>
           {headLabels.map((label, i) => (
-            <TableCell>{label}</TableCell>
+            <TableCell key={`head-label-${label}-${i}`}>{label}</TableCell>
           ))}
         </TableRow>
       </TableHead>
-      <TableBody></TableBody>
+      <TableBody>
+        {rows.map((row, i) => (
+          <RowBuilder
+            key={`row-builder-item-${JSON.stringify(row)}`}
+            rowData={row}
+            onDeleted={onRowUpdated}
+            onUpdated={onRowUpdated}
+          />
+        ))}
+      </TableBody>
     </Table>
   );
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,17 @@
-export const getHeadLabelsFromRows = (rows) =>
-  Array.isArray(rows) && rows.length > 0 && rows[0] && rows[0].data
+export const getHeadLabelsFromRows = (rows) => {
+  return Array.isArray(rows) && rows.length > 0 && rows[0] && rows[0].data
     ? [null, ...Object.keys(rows[0].data), null]
     : [];
+};
+
+export const getRowKids = (rowData) => {
+  if (!rowData || !rowData.kids) {
+    return null;
+  }
+
+  const key = Object.keys(rowData.kids).find(
+    (key) => rowData.kids[key] && rowData.kids[key].records
+  );
+
+  return key ? { key, records: rowData.kids[key].records } : null;
+};
